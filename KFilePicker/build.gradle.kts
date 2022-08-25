966a6ddc94eb27593cd746775f9bc5103345adcc
Tlaster <tlaster@outlook.com>
2022-08-25 13:02:17 +0800

    add gradle publish

--- KFilePicker/build.gradle.kts
+++ KFilePicker/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.*
+
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
@@ -78,5 +80,78 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+
+ext {
+    val publishPropFile = rootProject.file("publish.properties")
+    if (publishPropFile.exists()) {
+        Properties().apply {
+            load(publishPropFile.inputStream())
+        }.forEach { name, value ->
+            set(name.toString(), value)
+        }
+    } else {
+        set("signing.keyId", System.getenv("SIGNING_KEY_ID"))
+        set("signing.password", System.getenv("SIGNING_PASSWORD"))
+        set("signing.secretKeyRingFile", System.getenv("SIGNING_SECRET_KEY_RING_FILE"))
+        set("ossrhUsername", System.getenv("OSSRH_USERNAME"))
+        set("ossrhPassword", System.getenv("OSSRH_PASSWORD"))
+    }
+}
+
+val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    if (rootProject.file("publish.properties").exists()) {
+        signing {
+            sign(publishing.publications)
+        }
+        repositories {
+            maven {
+                val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                url = if (version.toString().endsWith("SNAPSHOT")) {
+                    uri(snapshotsRepoUrl)
+                } else {
+                    uri(releasesRepoUrl)
+                }
+                credentials {
+                    username = project.ext.get("ossrhUsername").toString()
+                    password = project.ext.get("ossrhPassword").toString()
+                }
+            }
+        }
+    }
+
+    publications.withType<MavenPublication> {
+        artifact(javadocJar)
+        pom {
+            name.set("KFilePicker")
+            description.set("KFilePicker")
+            url.set("https://github.com/Tlaster/KFilePicker")
+
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set("Tlaster")
+                    name.set("James Tlaster")
+                    email.set("tlaster@outlook.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/Tlaster/KFilePicker")
+                connection.set("scm:git:git://github.com/Tlaster/KFilePicker.git")
+                developerConnection.set("scm:git:git://github.com/Tlaster/KFilePicker.git")
+            }
+        }
     }
 }
